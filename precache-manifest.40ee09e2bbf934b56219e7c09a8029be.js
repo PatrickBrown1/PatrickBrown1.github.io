5bd12f64aa2da6aefb36e0a74208413f9ff50b7d
PatrickBrown1 <patrickbrown1312@gmail.com>
2020-10-05 17:08:00 -0700

    Updates

---
+++ precache-manifest.40ee09e2bbf934b56219e7c09a8029be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfec0d34318553cd7c592573228feac6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a97090fca86e0af0d212",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e3016619c9f4e94d1e1e",
+    "url": "/static/js/2.e72567c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e72567c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a97090fca86e0af0d212",
+    "url": "/static/js/main.9f458e6a.chunk.js"
+  },
+  {
+    "revision": "0339221a53aa7c5a7783",
+    "url": "/static/js/runtime-main.8a1e674c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
