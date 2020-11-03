4df14d5328b845df4c8e67bfef71b2c47d9d14d2
PatrickBrown1 <patrickbrown1312@gmail.com>
2020-11-02 22:27:57 -0800

    Created work in progress page

---
+++ src/WorkInProgressPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./WorkInProgressPage.css";
+function WorkInProgressPage() {
+  return (
+    <div className="body">
+      <h1 className="mainHeader">Work in Progress</h1>
+      <h2 className="subHeader">&mdash; come back soon &mdash;</h2>
+    </div>
+  );
+}
+export default WorkInProgressPage;
