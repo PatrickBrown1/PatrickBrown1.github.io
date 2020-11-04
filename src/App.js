59c1dff29930e2f5b97dd924340ea350d5fc1ad9
PatrickBrown1 <patrickbrown1312@gmail.com>
2020-11-03 18:52:53 -0800

    Started hero design with temp picture

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import WorkInProgressPage from "./WorkInProgressPage.js";
-
+import "./App.css";
+import PatrickImage from "./PatrickPicture.jpg";
 function App() {
   return (
     <div className="App">
-      <WorkInProgressPage />
+      <div className="hero-panel">
+        <div className="hero-item hero-left">
+          <img className="image" src={PatrickImage} />
+        </div>
+        <div className="hero-item hero-right">
+          <div className="text-box">
+            <h1 className="mainHeader">Patrick Brown</h1>
+            <h2 className="subHeader">Student | Programmer | Baker</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
