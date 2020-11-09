9887a050c88b004aa67ec9e211e6e70657c9dc53
PatrickBrown1 <patrickbrown1312@gmail.com>
2020-11-08 19:08:47 -0800

    Updated hero page css to be more understandable, better sectioning

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import PatrickImage from "./PatrickPicture.jpg";
 function App() {
   return (
     <div className="App">
-      <div className="hero-panel">
-        <div className="hero-item hero-left">
-          <img className="image" src={PatrickImage} />
-        </div>
-        <div className="hero-item hero-right">
-          <div className="text-box">
+      <div className="hero-view">
+        <div className="hero-panel">
+          <div className="image-container">
+            <img className="image" src={PatrickImage} />
+          </div>
+          <div className="text-container">
             <h1 className="mainHeader">Patrick Brown</h1>
             <h2 className="subHeader">Student | Programmer | Baker</h2>
           </div>
