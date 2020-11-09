a805a777e4a913eeb4e38ad683b1e67a6a1c7152
PatrickBrown1 <patrickbrown1312@gmail.com>
2020-11-08 19:35:18 -0800

    Added cool typing for header and subheader on load

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
 import React from "react";
 import WorkInProgressPage from "./WorkInProgressPage.js";
 import "./App.css";
+import Typist from 'react-typist';
 import PatrickImage from "./PatrickPicture.jpg";
+
+
+function MainHeader(){
+  return(
+    <Typist
+      cursor={{
+        show: false,
+        blink: true,
+        element: '|',
+        hideWhenDone: true,
+        hideWhenDoneDelay: 0,
+      }}
+      avgTypingDelay={100}
+    >
+      <Typist.Delay ms={1000} />
+      <h1 className="mainHeader">Patrick Brown</h1>
+    </Typist>
+  );
+}
+function SubHeader(){
+  return(
+    <Typist 
+      cursor={{
+        show: false,
+        blink: true,
+        element: '|',
+        hideWhenDone: true,
+        hideWhenDoneDelay: 0,
+      }}
+      avgTypingDelay={50}
+    >
+      <Typist.Delay ms={3000} />
+      <h2 className="subHeader">Student, Programmer, Baker</h2>
+    </Typist>
+  );
+  
+}
 function App() {
   return (
     <div className="App">
@@ -11,8 +49,8 @@ function App() {
             <img className="image" src={PatrickImage} />
           </div>
           <div className="text-container">
-            <h1 className="mainHeader">Patrick Brown</h1>
-            <h2 className="subHeader">Student | Programmer | Baker</h2>
+            <MainHeader />
+            <SubHeader />
           </div>
         </div>
       </div>
