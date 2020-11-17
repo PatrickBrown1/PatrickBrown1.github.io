0422493df72ce4869eb9a8db0dc1dfd39faadeb0
PatrickBrown1 <patrickbrown1312@gmail.com>
2020-11-16 22:20:31 -0800

    added on hover text appearence for link icons

--- src/LinkView.js
+++ src/LinkView.js
@@ -3,25 +3,31 @@ import "./LinkView.css";
 import LinkedInImage from "./linked-in-image.png";
 import GithubImage from "./github-image.png";
 import EmailImage from "./email-image.png";
+import {useSpring, animated} from 'react-spring'
 
 function LinkItem(props){
     const [showInfo, setShowInfo] = useState(false);
+    const rightPadding = showInfo ? 150 : 0;
+    const width = showInfo ? '10em' : '5em';
+    const sty = useSpring({paddingRight: rightPadding, maxWidth: width, minWidth: width, 
+      from: {paddingRight: 0, maxWidth: width, minWidth: width}})
     return(
-        <div className="link-item"
+        <animated.div style={{overflow: 'hidden', ...sty}} className="link-item"
           onMouseEnter={() => setShowInfo(true)} 
           onMouseLeave={() => setShowInfo(false)}
         >
             <img className="link-image" src={props.src} alt="Patrick Brown" />
-        </div>
+            {showInfo && <h1 className='link-text'>{props.name}</h1>}
+        </animated.div>
     );
 }
 
 function LinkView() {
   return (
       <div className="link-view">
-        <LinkItem name="linkedin" link="linkedin.com" src={LinkedInImage}/>
-        <LinkItem name="linkedin" link="linkedin.com" src={GithubImage}/>
-        <LinkItem name="linkedin" link="linkedin.com" src={EmailImage}/>
+        <LinkItem name="LinkedIn" link="linkedin.com" src={LinkedInImage}/>
+        <LinkItem name="GitHub" link="linkedin.com" src={GithubImage}/>
+        <LinkItem name="Email" link="linkedin.com" src={EmailImage}/>
       </div>
   );
 }
