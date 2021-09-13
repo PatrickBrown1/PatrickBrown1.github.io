cebacc58f3e0862500b6528397531f4e913f4b66
PatrickBrown1 <patrickbrown1312@gmail.com>
2021-09-12 18:53:04 -0700

    added responsive hero page

--- src/components/pages/Hero/Hero.jsx
+++ src/components/pages/Hero/Hero.jsx
@@ -1,10 +1,86 @@
 import React from 'react';
 import * as S from './Hero.styles';
 
-const Hero = () => {
+
+import PatrickImage from "../../../assets/PatrickPicture.jpg";
+
+// const MainHeader = () => {
+//   return(
+//     <Typist
+//       cursor={{
+//         show: false,
+//         blink: true,
+//         element: '|',
+//         hideWhenDone: true,
+//         hideWhenDoneDelay: 0,
+//       }}
+//       avgTypingDelay={100}
+//     >
+//       <Typist.Delay ms={500} />
+//       <h1 className="mainHeader">Patrick Brown</h1>
+//     </Typist>
+//   );
+// }
+// const SubHeader = () => {
+//   return(
+//     <Typist 
+//       cursor={{
+//         show: false,
+//         blink: true,
+//         element: '|',
+//         hideWhenDone: true,
+//         hideWhenDoneDelay: 0,
+//       }}
+//       avgTypingDelay={50}
+//     >
+//       <Typist.Delay ms={2500} />
+//       <h2 className="subHeader">Student, Programmer, Baker</h2>
+//     </Typist>
+//   );
+  
+// }
+// const App = () => {
+//   return (
+//     <div className="App">
+//       <div className="hero-view">
+//         <div className="hero-panel">
+//           <div className="image-container">
+//             <img className="image" src={PatrickImage} />
+//           </div>
+//           <div className="text-container">
+//             <MainHeader />
+//             <SubHeader />
+//           </div>
+//         </div>
+//       </div>
+//       <LinkView />
+//     </div>
+//   );
+// }
+const Hero = ({ windowSize }) => {
+    console.log(windowSize)
     return (
         <S.HeroContainer>
-            Hero
+            {windowSize.width >= 650 ?
+                <S.HeaderContainer>
+                    <S.ImageContainer>
+                        <S.Image src={PatrickImage} alt="Patrick Brown" />
+                    </S.ImageContainer>
+                    <S.HeaderText>
+                        <h1>Patrick Brown</h1>
+                        <h2>Experience About Me Resume Contact</h2>
+                    </S.HeaderText>
+                </S.HeaderContainer>
+            :
+                <S.HeaderText mobile={windowSize.width <= 650}>
+                    <h1>Patrick Brown</h1>
+                    <S.ImageContainer mobile={windowSize.width <= 650}>
+                        <S.Image src={PatrickImage} alt="Patrick Brown" />
+                    </S.ImageContainer>
+                    <h2>Experience About Me Resume Contact</h2>
+                </S.HeaderText>
+            }
+            
         </S.HeroContainer>
     );
 }
--- src/components/pages/Hero/Hero.styles.jsx
+++ src/components/pages/Hero/Hero.styles.jsx
@@ -3,11 +3,67 @@ import COLORS from '../../../constants/colors';
 
 export const HeroContainer = styled.div`
     position: relative;
-    display: block;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
     margin: 0;
     width: 100vw;
     height: 100vh; // needs to change
     
     box-sizing: border-box;
     border: 2px solid ${COLORS.green};
+`;
+
+export const HeaderContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+`;
+
+// .hero-panel {
+//   display: flex;
+//   flex-direction: row;
+//   justify-items: space-around;
+//   align-items: center;
+//   width: 75vw;
+//   margin-top: -20vh;
+// }
+export const ImageContainer = styled.div`
+    // min-width: 34%;
+    height: ${props => props.mobile ? '200px' : '300px'};
+    padding: 25px;
+`;
+export const Image = styled.img`
+    height: 100%;
+    border-radius: 50%;
+    box-shadow: 0px 0px 30px 2px rgba(0, 0, 0, 0.5);
+`;
+
+export const HeaderText = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center; 
+    padding: ${props => props.mobile ? '0' : '25px'};
+    
+    width: ${props => props.mobile ? '100vw' : '66%'};
+
+    color: ${COLORS.black};
+
+    h1 {
+        font-size: ${props => props.mobile ? '3em' : '4.2em'};
+        font-weight: 400;
+        font-family: Istok Web;
+        margin: ${props => props.mobile ? '15px 0' : '0'};;
+    }
+    h2 {
+        font-size: ${props => props.mobile ? '1.3em' : '1.5em'};
+        font-weight: 300;
+        font-family: Average Sans;
+        margin: ${props => props.mobile ? '15px 0' : '0'};;
+    }
 `;
